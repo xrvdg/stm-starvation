0c648210671978a62dd93889d24135886f15c1e0
Xander van der Goot <xander.vandergoot@channable.com>
2023-10-31 15:58:38 +0200

    Code in bug report

--- app/List.hs
+++ app/List.hs
@@ -1,36 +1,35 @@
 {-# LANGUAGE NumericUnderscores #-}
 
-import Control.Concurrent (myThreadId)
+import Control.Concurrent (ThreadId, myThreadId)
 import qualified Control.Concurrent.Async as Async
-import Control.Concurrent.STM (TVar, atomically, readTVar)
+import Control.Concurrent.STM (STM, TVar, atomically, readTVar)
 import Control.Concurrent.STM.TVar (newTVarIO)
-import Data.IORef (modifyIORef', newIORef, readIORef)
+import Data.IORef (IORef, modifyIORef', newIORef, readIORef)
 import GHC.IO (unsafePerformIO)
 import System.IO (hFlush, stdout)
-
-
-readTvars :: [TVar a] -> IO ()
-readTvars ltvar = do
-  threadId <- myThreadId
-  counter <- newIORef (0 :: Integer)
-  _ <- atomically (p threadId counter *> mapM readTVar ltvar)
-  return ()
-  where
-    p threadId counter = unsafePerformIO $ do
-      r <- readIORef counter
-      print (threadId, r)
-      hFlush stdout
-      modifyIORef' counter (+ 1)
-      return (return ())
 
 main :: IO ()
 main = do
   let l = [(x, x) | x <- [1 :: Integer .. 400_000]]
-  ltvar <- mapM newTVarIO l
-  let g = readTvars ltvar
-  let h = readTvars ltvar
-  let i = readTvars ltvar
-  g
-  print "done with g"
+  list <- mapM newTVarIO l
+  print "Initialized the List"
+  -- Read the list once to be sure it's fully evaluated
+  readTList list
+  Async.concurrently_ (readTList list) (readTList list)
+
+-- | irrevocable counter to keep track of transaction restart.
+restartCounter :: ThreadId -> IORef Integer -> STM ()
+restartCounter  threadId counter = unsafePerformIO $ do
+  r <- readIORef counter
+  print (threadId, r)
   hFlush stdout
-  Async.race_ h i
+  modifyIORef' counter (+ 1)
+  return (return ())
+
+readTList :: [TVar a] -> IO ()
+readTList ltvar = do
+  threadId <- myThreadId
+  counter <- newIORef (0 :: Integer)
+  atomically $ do
+    restartCounter threadId counter
+    mapM_ readTVar ltvar
--- app/Map.hs
+++ app/Map.hs
@@ -1,11 +1,9 @@
-{-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE NumericUnderscores #-}
-{-# LANGUAGE ScopedTypeVariables #-}
 
-import Control.Concurrent (myThreadId)
+import Control.Concurrent (ThreadId, myThreadId)
 import qualified Control.Concurrent.Async as Async
-import Control.Concurrent.STM (atomically)
-import Data.IORef (modifyIORef', newIORef, readIORef)
+import Control.Concurrent.STM (STM, atomically)
+import Data.IORef (IORef, modifyIORef', newIORef, readIORef)
 import GHC.IO (unsafePerformIO)
 import qualified ListT
 import qualified StmContainers.Map as STMMap
@@ -13,29 +11,31 @@ import System.IO (hFlush, stdout)
 
 main :: IO ()
 main = do
-  let (l :: [(Integer, Integer)]) = [(x, x) | x <- [1 .. 100_000]]
+  let l = [(x,x) | x <- [1 :: Integer .. 100_000]]
   stmmap <- STMMap.newIO
   atomically $ mapM_ (\(x, y) -> STMMap.insert x y stmmap) l
-  let g = readMap stmmap
-  let h = readMap stmmap
-  let i = readMap stmmap
-  g
-  print "done with g"
+  print "Initialized the STM Map."
   hFlush stdout
-  Async.race_ h i
+  -- Check that a read from a single thread does work
+  readMap stmmap
+  -- read the stm map from two threads concurrently
+  foldr1 Async.concurrently_ [readMap stmmap, readMap stmmap]
+
+-- | irrevocable counter to keep track of transaction restart.
+restartCounter :: ThreadId -> IORef Integer -> STM ()
+-- restartCounter threadId counter = pure ()
+restartCounter  threadId counter = unsafePerformIO $ do
+  r <- readIORef counter
+  print (threadId, r)
+  hFlush stdout
+  modifyIORef' counter (+ 1)
+  return (return ())
 
 readMap :: (Show a, Show b) => STMMap.Map a b -> IO ()
 readMap stmmap = do
   threadId <- myThreadId
   counter <- newIORef (0 :: Integer)
   _ <- atomically $ do
-    p threadId counter
+    restartCounter threadId counter
     ListT.toList (STMMap.listT stmmap)
   return ()
-  where
-    p threadId counter = unsafePerformIO $ do
-      r <- readIORef counter
-      print (threadId, r)
-      hFlush stdout
-      modifyIORef' counter (+ 1)
-      return (return ())
